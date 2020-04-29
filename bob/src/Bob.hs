d77517321102b82d2ac7224924fb41e459650444
Yuntong Qu <yuntong@utexas.edu>
2020-04-29 12:52:08 -0500

    add collatz

--- bob/src/Bob.hs
+++ bob/src/Bob.hs
@@ -1,24 +1,23 @@
 module Bob (responseFor) where
 import Data.Char as C
 
-checkQuestion :: String -> Bool 
-checkQuestion str = last (filter (/= ' ') str) == '?'
 
-checkYell :: String -> Bool
-checkYell "" = False
-checkYell str = allCapitalLetters $ filter C.isLetter str
-            where
-                allCapitalLetters xs =  all C.isUpper xs && not (null xs)
+allCapitalLetters :: String -> Bool
+allCapitalLetters xs =  all C.isUpper xs && not (null xs)
 
-checkSpace :: String -> Bool
-checkSpace str = all (== ' ') (dropWhile C.isSpace str)
 
 responseFor :: String -> String
-responseFor xs 
-            | checkYell xs &&  checkQuestion xs = "Calm down, I know what I'm doing!"
-            | checkYell xs  = "Whoa, chill out!"
-            | checkSpace xs = "Fine. Be that way!"
-            | checkQuestion xs = "Sure."
+responseFor str 
+            | checkYell &&  checkQuestion  = "Calm down, I know what I'm doing!"
+            | checkYell   = "Whoa, chill out!"
+            | checkSpace  = "Fine. Be that way!"
+            | checkQuestion  = "Sure."
             | otherwise = "Whatever."
+            where 
+                checkYell = allCapitalLetters $ filter C.isLetter str
+                checkSpace = all (== ' ') (dropWhile C.isSpace str)
+                checkQuestion = last (filter (/= ' ') str) == '?'
+       
+
 
 
